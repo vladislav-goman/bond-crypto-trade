db476c24efc8502300050fd2b13c4a496d85817b
Raman Samuilau <dronymys@mail.ru>
2022-03-10 00:25:37 +0100

    ido section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { TradingCards } from '../src/components/section/TradingCards';
 import { RatesSection } from '../src/components/section/RatesSection';
 import { CommunitySection } from '../src/components/section/CommunitySection';
 import { FundSection } from '../src/components/section/FundSection';
+import { IDOSection } from '../src/components/section/IDOSection';
 
 const Home: NextPage = () => {
   return (
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
         <RatesSection />
         <CommunitySection />
         <FundSection />
+        <IDOSection />
       </main>
     </>
   );
---
+++ src/components/section/IDOSection/IDOSection.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Image from 'next/image';
+import cx from 'classnames';
+import classes from './IDOSection.module.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Button, ButtonType } from '../../common/Button';
+import { Animation } from '../../common/Animation';
+import timer from '../../../images/timer.svg';
+import money from '../../../images/money-bill.svg';
+import hands from '../../../images/hands-helping.svg';
+
+export const IDOSection: React.FC = () => {
+  return (
+    <section id="ido-section">
+      <Container>
+        <h3 className={classes.ido__title}>Отдельный курс IDO</h3>
+        <Row>
+          <Col md={{ span: 4, offset: 2 }}>
+            <Animation className={'animate__fadeInLeft'}>
+              <div className={classes.ido__card}>
+                <p className={classes.ido__card__title}>
+                  Отдельный курс&nbsp;
+                  <span className={classes.ido__card__gradient}>IDO</span>
+                </p>
+                <p className={classes.ido__card__description}>
+                  Разработали и успешно проводим курс по инвестициям в проекты
+                  на ранней стадии развития, при поддержке экспертов в области
+                  IDO.
+                </p>
+                <div className={classes.buttonGroup}>
+                  <Button title="Записаться" />
+                  <Button title="Подробнее" type={ButtonType.secondary} />
+                </div>
+              </div>
+            </Animation>
+          </Col>
+          <Col md={{ offset: 5 }}>
+            <Animation className={'animate__fadeInRight'}>
+              <section className={classes.benefits}>
+                <div
+                  className={cx(classes.ido__card, classes.ido__card__small)}
+                >
+                  <Image src={timer} alt="timer" />
+                  <p className={classes.benefits__text}>2 недели обучения</p>
+                </div>
+                <div
+                  className={cx(classes.ido__card, classes.ido__card__small)}
+                >
+                  <Image src={money} alt="money bill" />
+                  <p className={classes.benefits__text}>
+                    Полное погружение в IDO
+                  </p>
+                </div>
+                <div
+                  className={cx(classes.ido__card, classes.ido__card__small)}
+                >
+                  <Image src={hands} alt="hands" />
+                  <p className={classes.benefits__text}>
+                    Доступ к аллокациям от нашего фонда
+                  </p>
+                </div>
+              </section>
+            </Animation>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
